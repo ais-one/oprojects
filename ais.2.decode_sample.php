274ec0cb95e1c20642b2bda38b502e9b33ec630b
Aaron Gong <ais-one@users.noreply.github.com>
2015-10-19 18:44:20 +0800

    Update ais.2.decode_sample.php
    Handle returned info as php objects

--- ais.2.decode_sample.php
+++ ais.2.decode_sample.php
@@ -8,7 +8,8 @@ $ais = new AIS();
 // Test Single Message
 if (1) {
 	$buf = "!AIVDM,1,1,,A,15DAB600017IlR<0e2SVCC4008Rv,0*64\r\n";
-	$ais->process_ais_buf($buf);
+	$o = $ais->process_ais_buf($buf);
+	var_dump($o);
 }
 
 // Test With Large Array Of Messages - represent packets of incoming data from serial port or IP connection
@@ -22,7 +23,10 @@ if (0) {
 		"\n!AIVDM,2,1,9,A,569w5`02>0V090=V221@DpN0<PV222222222221EC8S@:5O`0B4jCQhD,0*11\r\n!AIVDM,2,2,9,A,3lQH88888888880,2*6B\r\n!AIVDO,1,1,",
 		",A,D05GdR1MdffpuTf9H0,4*7","E\r\n!AIVDM,1,1,,A,?","8KWpp0kCm2PD00,2*6C\r\n!AIVDM,1,1,,A,?8KWpp1Cf15PD00,2*3B\r\nUIIII"
 	);
-	foreach ($test2_a as $test2_1) $ais->process_ais_buf($test2_1);
+	foreach ($test2_a as $test2_1) {
+		$o = $ais->process_ais_buf($test2_1);
+		var_dump($o);
+	}
 }
 
 ?>
